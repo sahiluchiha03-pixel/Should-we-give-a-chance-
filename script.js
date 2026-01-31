76c53330fbd011b7334bc7bc8b9a3f0c1aa6ae7d
sahiluchiha03-pixel <sahiluchiha03@gmail.com>
2026-01-31 17:09:22 +0530

    Add confirmation message functionality in script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+const messages = [
+  "Are you sure?",
+  "Really sure??",
+  "Think again 🥺",
+  "Don’t break my heart 💔",
+  "Last chance!",
+  "You know you want to 😏"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+  const noButton = document.querySelector(".no-button");
+  const yesButton = document.querySelector(".yes-button");
+
+  noButton.textContent = messages[messageIndex];
+  messageIndex = (messageIndex + 1) % messages.length;
+
+  const currentSize = parseFloat(
+    window.getComputedStyle(yesButton).fontSize
+  );
+
+  yesButton.style.fontSize = `${currentSize * 1.4}px`;
+}
+
+function handleYesClick() {
+  window.location.href = "yes.html";
+}
